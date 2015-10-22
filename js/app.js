d3cdc59a4278ce543a55848e0d09a3e74071b263
hazypaint <hazypaint@hotmail.com>
2015-10-22 18:00:48 +0200

    feat: markers are showing, still need to implement setVisible function

--- js/app.js
+++ js/app.js
@@ -4,34 +4,17 @@ var map;
 
 // location data
 var initialLocations = [
-    {name : "Basler Münster", address: "Rittergasse 3, 4051 Basel", tags: ["sight", "church", "cathedral"], lat: 47.556458, lng: 7.592443},
-    {name : "Zoo", address: "Binningerstrasse 40, 4054 Basel", tags: ["animals", "attraction", "family fun"], lat: 47.547405, lng: 7.578807},
-    {name : "Muesum of Art",  address: "St. Alban-Graben 16, 4051 Basel", tags: ["art", "museum"], lat: 47.554021, lng: 7.594268},
-    {name : "Railway Station", address: "Centralbahnstrasse 10, 4051 Basel", tags: ["getting here", "travel"], lat: 47.547571, lng: 7.589662},
-    {name : "St Paul's church", address: "Steinenring 20, 4051 Basel", tags: ["sight", "church", "cathedral"], lat: 47.551786, lng: 7.578393},
+    {name : "Basler Münster", address: "Rittergasse 3, 4051 Basel", tags: ["sight", "church", "cathedral"], lat: 47.556458, lng: 7.592443, marker: ""},
+    {name : "Zoo", address: "Binningerstrasse 40, 4054 Basel", tags: ["animals", "attraction", "family fun"], lat: 47.547405, lng: 7.578807, marker: ""},
+    {name : "Muesum of Art",  address: "St. Alban-Graben 16, 4051 Basel", tags: ["art", "museum"], lat: 47.554021, lng: 7.594268, marker: ""},
+    {name : "Railway Station", address: "Centralbahnstrasse 10, 4051 Basel", tags: ["getting here", "travel"], lat: 47.547571, lng: 7.589662, marker: ""},
+    {name : "St Paul's church", address: "Steinenring 20, 4051 Basel", tags: ["sight", "church", "cathedral"], lat: 47.551786, lng: 7.578393, marker: ""},
     ];
-
-// setting the constructor and ko.observables
-var Location = function(data) {
-    var self = this;
-    self.name = ko.observable(data.name);
-    self.address = ko.observable(data.address);
-    self.lat = ko.observable(data.lat);
-    self.lng = ko.observable(data.lng);
-    self.tags = ko.observableArray(data.tags);
+    
+    // log statement shows that markers are added
+    console.log(initialLocations);
 
 
-    // Marker for location
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(data.lat, data.lng),
-        map: map,
-        title: initialLocations.name,
-        visible: true
-    });
-
-    // Set the marker as a ko observable
-    self.marker = ko.observable(marker);
-};
 
 /****** VIEW *******/
 // initializing the google map
@@ -46,6 +29,10 @@ init = function() {
         title: initialLocations[0].name
     };
     map = new google.maps.Map(mapDiv, mapOptions);
+
+    for (var i = 0; i < initialLocations.length; i++) {
+    initialLocations[i].marker.setMap(map); 
+}
 };
 
 // Load map on page load
@@ -53,9 +40,25 @@ google.maps.event.addDomListener(window, 'load', function() {
     init();
 });
 
+
 /****** VIEWMODEL *******/
 var ViewModel = function() {
     var self = this;
+
+    // another map marker try
+    
+    // looping over initialLocations.marker and adding their data to the initial list
+    for (var i = 0; i < initialLocations.length; i++) {
+        initialLocations[i].marker = new google.maps.Marker({
+             position: new google.maps.LatLng(initialLocations[i].lat, initialLocations[i].lng),
+             title: '<p>' + initialLocations[i].name + '</p>',
+             visible: true,
+             map: map
+        });
+    } 
+
+    // To add the marker to the map, call setMap();
+    // initialLocations.marker.setMap(map); 
 
     // creating an array for the bullet points to display
     self.bulletPoints = ko.observableArray(initialLocations);
@@ -66,38 +69,13 @@ var ViewModel = function() {
     // creating a computed observable to find matches between user input and bullet point list 
     self.search = ko.computed(function(){
         return ko.utils.arrayFilter(self.bulletPoints(), function(bulletPoint){
-            return bulletPoint.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+            initialLocations[0].marker.setVisible(true);
+            // console.log(self.bulletPoints().marker);   
+            return bulletPoint.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;  
+
             });
+            
         });
-
-    var infowindow = new google.maps.InfoWindow({
-        content: '<div><h4 id="brewery-name"></h4><p id="brewery-address"></p></div>'
-      });
-
-    // making the locationsList an observable array;
-    self.locationList = ko.observableArray([]);
-    
-    // Creates an item for each location and pushes them to observable array
-    initialLocations.forEach(function(locationItem){
-        self.locationList.push( new Location(locationItem));
-    });
-
-    // Creats markers for each location and sets them on map on click event on name
-    self.setMarker = function(clickedLocation) {
-        clickedLocation.marker().setMap(map);
-    };
-
-    self.markerClick = function(location) {
-        console.log('1');
-        var contentString = '<div><h4 id="brewery-name">' + initialLocations.name() + '</h4>' +
-                      '<h5 id="brewery-address">' + initialLocations.address() + '</h5>' +
-                      '</div>';
-        infoWindow.setContent(contentString);
-
-        infowindow.open(map, initialLocations.marker);
-        console.log('window');
-      
-    };
 };
 
 // Apply bindings
