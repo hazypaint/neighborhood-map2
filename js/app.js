f2ac700a9cdefbb7ec6217b567f1bcd7057956b7
hazypaint <hazypaint@hotmail.com>
2015-11-05 16:07:40 +0100

    feat: NYT ajax request added

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var initialLocations = [
 {name : "Basler Münster", address: "Rittergasse 3, 4051 Basel", tags: ["church", "cathedral"], lat: 47.556458, lng: 7.592443, marker: null},
 {name : "Zoo Basel", address: "Binningerstrasse 40, 4054 Basel", tags: ["animals", "attraction", "family fun"], lat: 47.547405, lng: 7.578807, marker: null},
 {name : "Muesum of Art",  address: "St. Alban-Graben 16, 4051 Basel", tags: ["art", "museum"], lat: 47.554021, lng: 7.594268, marker: null},
-{name : "SBB Railway Station", address: "Centralbahnstrasse 10, 4051 Basel", tags: ["getting here", "travel"], lat: 47.547571, lng: 7.589662, marker: null},
+{name : "Basel SBB Railway Station", address: "Centralbahnstrasse 10, 4051 Basel", tags: ["getting here", "travel"], lat: 47.547571, lng: 7.589662, marker: null},
 {name : "St Paul's church", address: "Steinenring 20, 4051 Basel", tags: ["church", "cathedral"], lat: 47.551786, lng: 7.578393, marker: null},
 {name : "Museum Tinguely", address: "Paul Sacher-Anlage 2, 4002 Basel", tags: ["art", "museum"], lat: 47.559101, lng: 7.612236, marker: null},
 {name : "Theatre Basel", address: "Elisabethenstrasse 16, 4051 Basel", tags: ["art", "theatre", "shows"], lat: 47.553306, lng: 7.590082, marker: null},
@@ -48,24 +48,13 @@ google.maps.event.addDomListener(window, 'load', function() {
   init();
 });
 
-// not sure how to /if to use this yet
-// var Location = function(data){
-//   self.name = ko.observable(data.name);
-//   self.address = ko.observable(data.address);
-//   self.marker = ko.observable(data.marker);
-//   self.tags = ko.observableArray(data.tags);
-
-//   self.wikipediaArticle = ko.computed(function() {
-//     return self.name + " " + self.address;
-//   }, self);
-// }
-
 /****** VIEWMODEL *******/
 
 var ViewModel = function() {
   var self = this;  // = ViewModel{}
   var imageBlack = 'img/marker-icon-black.png';
   var $wikiElem = $('#wikipedia-links');
+  var $nytElem = $('#nytimes-articles');
 
   // creating an observable array for the bullet points to display
   self.bulletPoints = ko.observableArray(initialLocations);
@@ -90,14 +79,13 @@ var ViewModel = function() {
     var newContent = '<div id="content">'+
       '<div id="siteNotice">'+
       '</div>'+
-      '<h2 id="firstHeading" class="firstHeading">' + self.bulletPoints()[i].name + '</h2>'+
+      '<h3 id="firstHeading" class="firstHeading">' + self.bulletPoints()[i].name + '</h3>'+
       '<div id="bodyContent">'+
       '<p>The <b>' + self.bulletPoints()[i].name + '</b> is located at <b>' + self.bulletPoints()[i].address + '</b></p>'+
-      // API input will go here at some point
-      '<h4>Wikipedia Articles:</h4>' + 
-      '<ul id="wikipedia-links"></ul>' +
-      // '<p>More information from Wikipedia: <a href="https://en.wikipedia.org/w/index.php?title=' + self.bulletPoints()[i].name + '">'+
-      // 'https://en.wikipedia.org/w/index.php?title=' + self.bulletPoints()[i].name + '</a> '+
+      '<h5>Wikipedia Articles:</h5>' + 
+      // needs fix: should update wikipedia link 
+      '<ul id="wikipedia-links">xx</ul>' +
+      '<h5>Foursquare rating:</h5>' + 
       '</p>'+
       '</div>'+
       '</div>';
@@ -113,7 +101,7 @@ var ViewModel = function() {
       };
     })(newContent));
 
-    // more Google Maps events
+    // other Google Maps events
     google.maps.event.addListener(newMarker, 'click', (function(contentCopy) {
       return function() {
         if (contentCopy.getAnimation() !== null) {
@@ -123,25 +111,26 @@ var ViewModel = function() {
           // makes marker bounce once upon click
           contentCopy.setAnimation(google.maps.Animation.BOUNCE);
           setTimeout(function(){ contentCopy.setAnimation(null); }, 750);
+          // clears wiki and NYT text before loading new content
           $wikiElem.text("");
-          console.log('load');    // loads each time marker or list item is clicked
+          $nytElem.text("");
+          // loads wiki content related to clicked marker or list item
           loadData(contentCopy);
         };
       }
     })(newMarker));
-
   }
     
   var loadData = function (bulletPoint){
-    // for (var i = 0; i < self.bulletPoints().length; i++) {
-      console.log(bulletPoint.title);
-      var requestWiki  = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+ bulletPoint.title + '&format=json&callback=wikiCallback';
-    // }
-    console.log(requestWiki); 
+    // wikipedia link created with wiki sandbox
+    var requestWiki  = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+ bulletPoint.title + '&format=json&callback=wikiCallback';
+    // NYTimes link for request
+    var requestNYT = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + bulletPoint.title + "&api-key=bec2df7e772a904f941747a02d2bc3e7:4:72782796";
+
     // time out error info in case wikipedia can't be loaded
-    // var wikiRequestTimeout = setTimeout(function(){
-    //     $wikiElem.text("Wikipedia articles could not be loaded.");
-    // }, 8000);
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("Wikipedia articles could not be loaded.");
+    }, 8000);
 
     // Wikipedia AJAX request
     $.ajax({
@@ -156,16 +145,32 @@ var ViewModel = function() {
             var url = "http://en.wikipedia.org/wiki/" + wikiArticles[i];
             $wikiElem.append("<li><a href='" + url + "'target='_blank'>" + site + "</a></li>" );
             }
-            // };
-          // }
-        // clearTimeout(wikiRequestTimeout);
+        clearTimeout(wikiRequestTimeout);
       },
       error: function (e) {
         console.log('error');
           $wikiElem.text("Wikipedia articles could not be loaded.");
       }
     });
+    
 
+    // NYTimes AJAX request
+    $.getJSON(requestNYT)
+      .done ( function (data) {
+        // $nytHeaderElem.text("New York Times articles about: " + cityValue);
+        articles = data.response.docs;
+
+        //runs through each article and provides the requested data from each
+        for (var i = 0 ; i < articles.length ; i++ ) {
+            var headlineAndLink = "<a href='" + articles[i].web_url + "'target='_blank''>" + articles[i].headline.main;
+            var snippet = "<p>" + articles[i].snippet + "</p>" ;
+            $nytElem.append("<li class='article'>" + headlineAndLink  + "</a>" + snippet + "</li>" );
+            };
+      })
+
+      .fail ( function (e) {
+          $nytElem.text("New York Times article could not be loaded.");
+      });
     return false;
   };
   
@@ -192,7 +197,6 @@ var ViewModel = function() {
     var listMarker = bulletPoint.marker;
     google.maps.event.trigger(listMarker, "click");
   };  
-
 };
 
 // Apply bindings
